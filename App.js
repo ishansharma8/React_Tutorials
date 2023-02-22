fc18e7b5fa1bd6c9bc1e9cf9bd9a34ddc71c978e
ishansharma8 <ishansharma11228@gmail.com>
2023-02-23 01:18:31 +0530

    Printing hello everyone using react

---
+++ App.js
@@ -0,0 +1,3 @@
+const heading = React.createElement("h1", {}, "Namaste everyone 🦊");
+const root = ReactDOM.createRoot(document.getElementById("root")); //i want to run in div
+root.render(heading);
