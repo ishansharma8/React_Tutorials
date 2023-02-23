eb944a6d4dbcf4a575bf1d9889b52236cc5f1a9f
ishansharma8 <ishansharma11228@gmail.com>
2023-02-23 19:48:15 +0530

    Revert "how arrow fxns work differently"
    This reverts commit 9a52dc3f4ec3ed3fd66d90f53fd4ef73fc3cd0f2.

--- 2_Arrowfxn.js
+++ 2_Arrowfxn.js
@@ -1,14 +1,23 @@
-// console.log(this);
+function x()
+{
+    const a =10;
+}
+var xyz=10;
 
-const obj={
-    fn:function(){
-        console.log(this);  //here this refers to obj
-    },
-    fn2:()=>{
-        console.log(this);// in case of arrow fxn this refers to parent's this
-    }
+x(); //fxn execution context is created
+
+var x=function(){         //anonymous fxn
+    console.log(" i am an anonymous fxn");
 }
 
+<<<<<<< HEAD
 obj.fn(); //op- obj         this fxnc is called by obj.fn
 obj.fn2();//op- window
 //whenever you see this it depends how u are calling that fxn
+=======
+//in ES6 arrow fxns came in . let const spread operator,promises also came in
+
+const fn=()=>{  //kind of resembles to lambda fxn
+
+}
+>>>>>>> parent of 9a52dc3 (how arrow fxns work differently)
