9a52dc3f4ec3ed3fd66d90f53fd4ef73fc3cd0f2
ishansharma8 <ishansharma11228@gmail.com>
2023-02-23 19:37:14 +0530

    how arrow fxns work differently

--- 2_Arrowfxn.js
+++ 2_Arrowfxn.js
@@ -1,17 +1,14 @@
-function x()
-{
-    const a =10;
+// console.log(this);
+
+const obj={
+    fn:function(){
+        console.log(this);  //here this refers to obj
+    },
+    fn2:()=>{
+        console.log(this);// in case of arrow fxn this refers to parent's this
+    }
 }
-var xyz=10;
 
-x(); //fxn execution context is created
-
-var x=function(){         //anonymous fxn
-    console.log(" i am an anonymous fxn");
-}
-
-//in ES6 arrow fxns came in . let const spread operator,promises also came in
-
-const fn=()=>{  //kind of resembles to lambda fxn
-
-}
+obj.fn(); //op- obj        this fxn is called by obj.fn
+obj.fn2();//op- window
+//whenever you see this it depends how u are calling that fxn
