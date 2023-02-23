1a082f7137dde481a8b966641407e4951ac9571e
ishansharma8 <ishansharma11228@gmail.com>
2023-02-23 19:22:41 +0530

    arrow fxn intro

---
+++ 2_Arrowfxn.js
@@ -0,0 +1,17 @@
+function x()
+{
+    const a =10;
+}
+var xyz=10;
+
+x(); //fxn execution context is created
+
+var x=function(){         //anonymous fxn
+    console.log(" i am an anonymous fxn");
+}
+
+//in ES6 arrow fxns came in . let const spread operator,promises also came in
+
+const fn=()=>{  //kind of resembles to lambda fxn
+
+}
