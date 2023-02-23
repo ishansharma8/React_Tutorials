9cf851cc869d2d09edec4897448a3291aceea26e
ishansharma8 <ishansharma11228@gmail.com>
2023-02-23 20:22:42 +0530

    How u call a fxn determines this

--- 2_Arrowfxn.js
+++ 2_Arrowfxn.js
@@ -1,23 +1,15 @@
-function x()
-{
-    const a =10;
+const person1={
+    name:"aman"
 }
-var xyz=10;
-
-x(); //fxn execution context is created
-
-var x=function(){         //anonymous fxn
-    console.log(" i am an anonymous fxn");
+const person2={
+    name:"sam"
+}
+function x(){
+    console.log(this)
 }
 
-<<<<<<< HEAD
-obj.fn(); //op- obj         this fxnc is called by obj.fn
-obj.fn2();//op- window
-//whenever you see this it depends how u are calling that fxn
-=======
-//in ES6 arrow fxns came in . let const spread operator,promises also came in
+x();   //this and below are same
+x.call(this);  //window
+x.call(person);//aman
+x.call(person2);//sam
 
-const fn=()=>{  //kind of resembles to lambda fxn
-
-}
->>>>>>> parent of 9a52dc3 (how arrow fxns work differently)
